bb6f65f8d4c8ee16aeb311126bd5ec0e28bc71a2
jarhun <qqqappp89@naver.com>
2015-09-30 20:48:30 +0900

    node js 추가

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req,res) {
-  res.send('Hello World!');
-});
+app.set("view engine", 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function(){
   console.log('Server On!');
